85e93e0ff2255bcf6da60c495df91d7f6d782461
willburhachita <wilburhachita@mail.com>
2025-08-02 20:59:47 +0200

    update deployment

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,9 +6,9 @@ interface LogoProps {
 
 export const Logo: React.FC<LogoProps> = ({ className = "w-8 h-8" }) => {
   return (
-    <img 
-      src="/src/data/images/Adobe Express - file.png" 
-      alt="Munda Wanga Real Estate Logo" 
+    <img
+      src="/logo.png"
+      alt="Munda Wanga Real Estate Logo"
       className={className}
     />
   );
